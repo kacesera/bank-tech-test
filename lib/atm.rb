5fa7ccb82d98d08aebceb99a27e84ac2338c4012
KC Thomas <thisiskcthomas@gmail.com>
2020-12-01 14:52:41 +0000

    Clients can make deposits at the ATM.

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,3 +1,15 @@
 class ATM
 
+  def initialize
+    @balance = 0
+  end
+
+  def deposit(cash)
+    @balance += cash
+  end
+
+  def check_balance
+    @balance
+  end
+
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -2,4 +2,11 @@ require 'atm'
 
 describe ATM do 
 
+  describe "#deposit" do
+    it "adds money to the balance" do
+      atm = ATM.new
+      atm.deposit(1000)
+      expect(atm.check_balance).to eq 1000
+    end
+  end
 end
