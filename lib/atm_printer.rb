61ff206bc44bb77cc44d0638ab3667ec7bc4edd7
KC Thomas <thisiskcthomas@gmail.com>
2020-12-01 16:40:25 +0000

    Extracted ATMPrinter class and tests.

--- lib/atm_printer.rb
+++ lib/atm_printer.rb
@@ -1,3 +1,19 @@
 class ATMPrinter 
 
+  def self.print(transaction_history)
+    puts "date || credit || debit || balance"
+    transaction_history.each do |transaction|
+      if transaction.has_key?(:credit)
+        puts "#{transaction[:date]} || #{format_to_currency(transaction[:credit])} || || #{format_to_currency(transaction[:balance])}"
+      else
+        puts "#{transaction[:date]} || || #{format_to_currency(transaction[:debit])} || #{format_to_currency(transaction[:balance])}"
+      end
+    end
+  end
+
+  private
+
+  def self.format_to_currency(num)
+    '%.2f' %  num
+  end
 end
--- spec/atm_printer_spec.rb
+++ spec/atm_printer_spec.rb
@@ -1,5 +1,28 @@
 require 'atm_printer'
 
 describe ATMPrinter do
-
+  let(:transaction_history) {
+    [{
+      debit: 500,
+      date: '14/01/2012',
+      balance: 2500
+    },
+    {
+      credit: 2000,
+      date: '13/01/2012',
+      balance: 3000
+    },
+    {
+      credit: 1000,
+      date: '10/01/2012',
+      balance: 1000
+    }]}  
+  
+    describe '#print' do
+    it "outputs the transaction history" do
+      expect{ ATMPrinter.print(transaction_history) }.to output(
+        "date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00\n13/01/2012 || 2000.00 || || 3000.00\n10/01/2012 || 1000.00 || || 1000.00\n"
+      ).to_stdout
+    end
+  end
 end
