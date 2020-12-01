dd64590ab495dc8b1392692e9401741b1727b5bc
KC Thomas <thisiskcthomas@gmail.com>
2020-12-01 14:54:06 +0000

    Balance starts at 0

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -2,6 +2,10 @@ require 'atm'
 
 describe ATM do 
 
+  it "starts with a balance of 0" do
+    expect(ATM.new.check_balance).to eq 0
+  end
+  
   describe "#deposit" do
     it "adds money to the balance" do
       atm = ATM.new
