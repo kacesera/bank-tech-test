02ab7a00a82bbab54bc112111f6e04792b2b7517
KC Thomas <thisiskcthomas@gmail.com>
2020-12-01 16:43:32 +0000

    Extract private methods (#self.print_credit, #self.print_debit) from #self.print

--- lib/atm_printer.rb
+++ lib/atm_printer.rb
@@ -3,15 +3,20 @@ class ATMPrinter
   def self.print(transaction_history)
     puts "date || credit || debit || balance"
     transaction_history.each do |transaction|
-      if transaction.has_key?(:credit)
-        puts "#{transaction[:date]} || #{format_to_currency(transaction[:credit])} || || #{format_to_currency(transaction[:balance])}"
-      else
-        puts "#{transaction[:date]} || || #{format_to_currency(transaction[:debit])} || #{format_to_currency(transaction[:balance])}"
-      end
+      self.print_credit(transaction) if transaction.has_key?(:credit)
+      self.print_debit(transaction) if transaction.has_key?(:debit)
     end
   end
 
   private
+
+  def self.print_credit(transaction)
+    puts "#{transaction[:date]} || #{format_to_currency(transaction[:credit])} || || #{format_to_currency(transaction[:balance])}"
+  end
+
+  def self.print_debit(transaction)
+    puts "#{transaction[:date]} || || #{format_to_currency(transaction[:debit])} || #{format_to_currency(transaction[:balance])}"
+  end
 
   def self.format_to_currency(num)
     '%.2f' %  num
