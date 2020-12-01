c0ddec4ac6361720b479157827f398618b1a7fed
KC Thomas <thisiskcthomas@gmail.com>
2020-12-01 16:44:32 +0000

    Extract private method (#self.print_header) from #self.print

--- lib/atm_printer.rb
+++ lib/atm_printer.rb
@@ -1,7 +1,7 @@
 class ATMPrinter 
 
   def self.print(transaction_history)
-    puts "date || credit || debit || balance"
+    self.print_header
     transaction_history.each do |transaction|
       self.print_credit(transaction) if transaction.has_key?(:credit)
       self.print_debit(transaction) if transaction.has_key?(:debit)
@@ -9,6 +9,10 @@ class ATMPrinter
   end
 
   private
+
+  def self.print_header
+    puts "date || credit || debit || balance"
+  end
 
   def self.print_credit(transaction)
     puts "#{transaction[:date]} || #{format_to_currency(transaction[:credit])} || || #{format_to_currency(transaction[:balance])}"
