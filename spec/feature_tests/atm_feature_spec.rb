534309ad0f304d17d94ad168a0b488990ba0f6c0
KC Thomas <thisiskcthomas@gmail.com>
2020-12-03 22:01:28 +0000

    Feature test added for printing transaction history

--- spec/feature_tests/atm_feature_spec.rb
+++ spec/feature_tests/atm_feature_spec.rb
@@ -5,6 +5,7 @@ describe 'ATM' do
   
   before :each do
     atm.deposit(500)
+    @today = Date.today.strftime('%d/%m/%Y')
   end
 
   it 'lets user deposit money' do
@@ -14,5 +15,10 @@ describe 'ATM' do
   it 'lets user withdraw money' do
     atm.withdraw(25)
     expect(atm.check_balance).to eq 475
+  end
+
+  it 'shows the user their transaction history' do
+    atm.withdraw(500)
+    expect{ atm.print_transaction_history }.to output("date || credit || debit || balance\n#{@today} || || 500.00 || 0.00\n#{@today} || 500.00 || || 500.00\n").to_stdout
   end
 end
